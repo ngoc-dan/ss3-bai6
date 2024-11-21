486c8b27a68849ab47e9a5607d8a291e55fec1cc
ngoc-dan <ngocdan191026@gmail.com>
2024-11-22 06:46:35 +0700

    Add files via upload

---
+++ 3.6.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+	float a,h,dientich;
+	 printf("nhap a: ");
+	scanf("%f", &a);
+	printf("nhap h: ");
+	scanf("%f", &h);
+	dientich = a * h / 2;
+	printf("%f", dientich);
+	
+	return 0;
+}
